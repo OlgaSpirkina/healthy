afd53d4121b2db8f9fbf497aeb445c361bc57e9e
olga <>
2021-07-12 13:27:01 +0200

    add project js manipulate a table of objects

--- assets/script/array.js
+++ assets/script/array.js
@@ -1,17 +1,17 @@
 // un tableau d'objets
-let companiesList = [
+let recipesList = [
   {
-    id: 1,
+    id: 0,
     name: 'Avocado toast',
-    level: 'facile',
+    level: 1,
     ingredients:['une tranche de pain', 'un avocado mur', 'un oeuf dur', 'huile d\'olive', 'graines de tournesole'],
     coockingTime: '15 min',
     img: 'assets/img/avocado.jpg'
   },
   {
-    id: 2,
+    id: 1,
     name: 'Salade de Tomates',
-    level: 'facile',
+    level: 1,
     ingredients:['tomates frèches', 'jeunes pouces de salade', 'la roquette',
     'graines de tournesole', 'huile d\'olive',
     'olives sans noyau'
@@ -20,9 +20,9 @@ let companiesList = [
     img: 'assets/img/tomato-bowl.jpg'
   },
   {
-    id: 3,
+    id: 2,
     name: 'Quinoa bowl',
-    level: 'facile',
+    level: 2,
     ingredients:['Quinoa', 'concombre', 'poivron rouge',
     'cernaux de noix', 'percile',
     'huile de colza'
@@ -31,9 +31,9 @@ let companiesList = [
     img: 'assets/img/quinoa.jpg'
   },
   {
-    id:4,
+    id:3,
     name: 'Soupe de butternut à la truffe',
-    level: "moyen",
+    level: 3,
     ingredients: ['Courge butternut', 'sauce à la truffe', 'oignons',
     'feuilles de basilic', 'huile d\'olive'
     ],
@@ -41,9 +41,9 @@ let companiesList = [
     img: 'assets/img/butternut.jpg'
   },
   {
-    id: 5,
+    id: 4,
     name: 'Salade de crudités',
-    level: 'facile',
+    level: 1,
     ingredients: ['Concombre', 'poivron jaune et rouge', 'oignon',
     'olives sans noyau', 'jeunes pousses de salade',
     'huile de colza'
@@ -52,9 +52,9 @@ let companiesList = [
     img: 'assets/img/crudites.jpg'
   },
   {
-    id: 6,
+    id: 5,
     name: 'Curry veggi',
-    level: 'chef',
+    level: 4,
     ingredients: ['Carotte', 'pommes de terre',
     'aubergine', 'courgette', 'lait de coco',
     'pois chiche', 'd\'ail'
@@ -63,9 +63,9 @@ let companiesList = [
     img: 'assets/img/curry.jpg'
   },
   {
-    id: 7,
+    id: 6,
     name: 'Salade aux falafels',
-    level: 'moyen',
+    level: 3,
     ingredients: ['Pois chiche', 'poivron rouge',
     'jeunes pousses', 'choux rouge', 'percile',
     'carottes', 'oignon', 'huile d\'olive'
@@ -74,9 +74,9 @@ let companiesList = [
     img: 'assets/img/falafel.jpg'
   },
   {
-    id: 8,
+    id: 7,
     name: 'Asperge et tomates-cerise',
-    level: 'facile',
+    level: 1,
     ingredients:   ['Asperge', 'tomates-cerise',
     'oeuf dur', 'persile',
     'huile de colza'
@@ -85,9 +85,9 @@ let companiesList = [
     img: 'assets/img/asperge.jpg'
   },
   {
-    id: 9,
+    id: 8,
     name: 'Toast aux figues fraiches',
-    level: 'facile',
+    level: 1,
     ingredients: ['Pain complet', 'figues fraiches',
     'fromage de chevre en tranche', 'graines de courge',
     'feuilles de mente'
@@ -100,36 +100,28 @@ let companiesList = [
 const input = document.getElementById("searchBar");
 input.addEventListener('keyup', (e) => {
   const searchString = e.target.value.toLowerCase();
-  const filteredCompanies = companiesList.filter(eachCompany => {
+  const filteredRecipes = recipesList.filter(eachRecipe => {
     return (
-      eachCompany.name.toLowerCase().includes(searchString) || eachCompany.post.toLowerCase().includes(searchString)
+      eachRecipe.name.toLowerCase().includes(searchString)
+      ||
+      eachRecipe.ingredients.toString().toLowerCase().includes(searchString) //je convertis le tableau d'ingredients en string
     );
   });
   // j'appelle la fonction qui génére les cartes avec la fonction qui filtre comme argument
-  displayCharacters(filteredCompanies);
+  displayRecipesAsCards(filteredRecipes);
 
 });
 // la fonction qui parcours un tableau et permet d'afficher les elements du tableau dans le html en forme d'une carte bootstrap
-const displayCharacters = (anyArray) => {
-  const today = new Date();
+const displayRecipesAsCards = (anyArray) => {
   const htmlString = anyArray.map((arrayElement) => {
-    // début de la fonction qui calcule les jours passés du moment de l'application
-      var application = new Date(arrayElement.coockingTime);
-      var differenceInTime = today.getTime() - application.getTime();
-      var differenceInDays = parseInt(differenceInTime / (1000 * 3600 * 24));
-      differenceInDays = (
-        differenceInDays == 0 ? 'postulé aujourd\'hui' : differenceInDays == 1 ? 'postulé il y a ' + differenceInDays + ' jour' : 'postulé il y a ' + differenceInDays + ' jours'
-      );
-// la fin de la fonction calculant les jours passés et récupération de la variable differenceInDays
-
 // la carte qui sera afficher pour chaque element du tableau
     return `
     <div id="item_${arrayElement.id}" class="card m-3" style="width: 30rem;">
      <img src="${arrayElement.img}" class="card-img-top img-fluid" style="height: 20rem;" alt="logo de la sociéte ${arrayElement.name}">
       <div class="card-body">
         <h5 class="card-title mb-5">${arrayElement.name}</h5>
-        <p class="card-text m-3">${arrayElement.coockingTime}</p>
-        <button class="btn btn-primary" onclick="displayPopup('${arrayElement.name}', '${arrayElement.post}', '${arrayElement.id}', '${arrayElement.website}')">Consulter</button>
+        <p class="card-text m-3"><i class="fas fa-3x fa-clock"></i><span class="mx-2">${arrayElement.coockingTime}</span></p>
+        <button class="btn btn-primary" onclick="displayPopup('${arrayElement.name}', '${arrayElement.level}', '${arrayElement.id}')">Consulter</button>
       </div>
     </div>
     `;
@@ -138,36 +130,40 @@ const displayCharacters = (anyArray) => {
   document.getElementById("displayCards").innerHTML = htmlString;
 }
 // j'appelle la fonction qui va trier / searchBar
-displayCharacters(companiesList);
+displayRecipesAsCards(recipesList);
 
 // afficher l'onglet avec plus de détaille consernant chaque offre. L'onglet est composé des divs stylisées
 // elle est appeller au moment de génération des cartes (à la ligne 167)
-function displayPopup(name, post, id, web){
-      companiesList.forEach((item) => {
+function displayPopup(name, level, id){
+      recipesList.forEach((item) => {
         if(item.name == name){
           let answer =
           `
           <div id="popup" class="popup">
             <div id="popup-heading">
               <i class="far fa-2x fa-window-close m-2" onclick="closeIt()"></i>
-              <p class="m-2"><small>Savoir plus sur le post</small></p>
+              <p class="m-2"><small>Savoir plus sur les ingrédients</small></p>
             </div>
             <div id="popup-title">
               <h1>${name}</h1>
-              <h2>${post}</h2>
+              <div>
+                <h2 id="lemon-level">Niveau: </h2>
+              </div>
             </div>
             <div id="popup-body">
+              <h3 class="m-2">Ingrédients</h3>
               <ul id="displayTechnos">
               </ul>
             </div>
             <div id="popup-footer">
-              <a href="${web}" target="_blank" class="btn m-2">Consulter l'offre</a>
+              <a href="#" target="_blank" class="btn m-2"><i class="fas fa-3x fa-share-alt-square"></i><small>partager!</small></a>
             </div>
           </div>
           `;
           document.getElementById('displayPopup').innerHTML += answer;
-          // j'appelle la fonction qui affiche toutes les technologies utilisées en tant que la list non-ordonnée
+          // j'appelle la fonction qui affiche toutes les ingredients utilisées en tant que la list non-ordonnée
           displayList(`${id}`);
+          levelFunction(`${id}`);
         }
       });
 }
@@ -176,8 +172,26 @@ function displayPopup(name, post, id, web){
 function closeIt(){
   document.getElementById('displayPopup').innerHTML = "";
 }
-let displayList = (ids) =>{
-  companiesList[ids].technologies.forEach((item) => {
-    document.getElementById('displayTechnos').innerHTML += `<li class="mx-auto">${item}</li>`;
-})
+// pour afficher la liste des ingredients utilisées je parcours le tableau de la clé recipesList.ingredients
+let displayList = (index) =>{
+  recipesList.forEach((item) => {
+    if(index == item.id){
+      console.log(item.ingredients);
+      item.ingredients.forEach((elems) => {
+          document.getElementById('displayTechnos').innerHTML += `<li class="mx-auto">${elems}</li>`;
+      });
+    }
+  });
+}
+let levelFunction = (index) =>{
+  recipesList.forEach((item) => {
+    if(index == item.id){
+      for(let i=1; i<=item.level; i++){
+        document.getElementById('lemon-level').innerHTML += `<i class="fas fa-2x fa-lemon"></i>`;
+      }
+    }
+
+
+  });
+
 }
